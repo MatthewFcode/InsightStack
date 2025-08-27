d1af2c3161f45a2729eba4894751d5466a1b808d
Matthew Foley <matthewfoley333@gmail.com>
2025-08-27 22:29:51 +1200

    feat: add the form text fields and the handle post logic in the tech component

--- client/components/Tech.tsx
+++ client/components/Tech.tsx
@@ -1,10 +1,44 @@
+import { useState } from 'react'
+import {
+  useGetPosts,
+  useAddPosts,
+  useUpdatePosts,
+  useDeletePosts,
+} from '../hooks/usePosts.ts'
+
 function Tech() {
+  const addMutation = useAddPosts()
+  const updatePosts = useUpdatePosts()
+  const deleteMutation = useDeletePosts()
+
+  const { data, isLoading, isError } = useGetPosts()
+
+  // state for the topic field
+  const [topicInput, setTopicInput] = useState('')
+  // state for the post details field
+  const [detailsInput, setDetailsInput] = useState('')
+  // post button logic
+  const handlePost = async (event) => {
+    await addMutation.mutateAsync({
+      topic: topicInput,
+      postDetails: detailsInput,
+    })
+    setTopicInput('')
+    setDetailsInput('')
+  }
   return (
     <>
       <div>
         <h1>Tech</h1>
       </div>
-      <div></div>
+      <div>
+        <form>
+          <label></label>
+          <input type="text" placeholder="Whats you're topic?" />
+          <label></label>
+          <input type="text" placeholder="What are you're tips?" />
+        </form>
+      </div>
     </>
   )
 }
