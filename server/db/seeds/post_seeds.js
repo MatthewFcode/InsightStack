99cd04ec394d4ed477bac7ad394c62e67f590121
Matthew Foley <matthewfoley333@gmail.com>
2025-08-28 16:24:36 +1200

    krbkar

--- server/db/seeds/post_seeds.js
+++ server/db/seeds/post_seeds.js
@@ -7,7 +7,6 @@ export async function seed(knex) {
   await knex('posts').del()
   await knex('posts').insert([
     {
-      id: 1,
       topic: 'Seeds with knex.js',
       post_details:
         'use .trucate() instead of .del() to reset auto incrementing ids. Also there is no down function for seeds the logic for deleting existing table data is written in your seed and then your seed data runs directly after that.',
