85615403c75a6571d79b335fc20625320cde4fb6
Matthew Foley <matthewfoley333@gmail.com>
2025-09-11 11:47:42 +1200

    feat: add the server routes

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ import * as Path from 'node:path'
 import skillRoutes from './routes/skills.ts'
 import newPostRoutes from './routes/posts.ts'
 import userRoutes from './routes/users.ts'
+import favRoutes from './routes/fav-votes.ts'
 
 const server = express()
 
@@ -13,7 +14,7 @@ server.use(express.static(Path.resolve('public')))
 server.use('/api/v1/posts', newPostRoutes)
 server.use('/api/v1/skills', skillRoutes)
 server.use('/api/v1/users', userRoutes)
-server.use('/api/v1/fav-languages')
+server.use('/api/v1/fav-languages', favRoutes)
 server.use('/api/v1/least-fav-languages')
 
 if (process.env.NODE_ENV === 'production') {
