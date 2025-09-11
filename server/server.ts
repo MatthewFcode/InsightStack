ddecf9d7e6983da2a4e0f6f4656c19fd6273cdd4
Matthew Foley <matthewfoley333@gmail.com>
2025-09-11 14:14:29 +1200

    refactor back end code

--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,7 @@ import skillRoutes from './routes/skills.ts'
 import newPostRoutes from './routes/posts.ts'
 import userRoutes from './routes/users.ts'
 import favRoutes from './routes/fav-votes.ts'
+import leastFavRoutes from './routes/non-fav-votes.ts'
 
 const server = express()
 
@@ -15,7 +16,7 @@ server.use('/api/v1/posts', newPostRoutes)
 server.use('/api/v1/skills', skillRoutes)
 server.use('/api/v1/users', userRoutes)
 server.use('/api/v1/fav-languages', favRoutes)
-server.use('/api/v1/least-fav-languages')
+server.use('/api/v1/least-fav-languages', leastFavRoutes)
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
