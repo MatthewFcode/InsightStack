441c0759470fd4adba5ee0c160df2fb208fc239e
Matthew Foley <matthewfoley333@gmail.com>
2025-09-14 19:48:03 +1200

    refactor: comment out fav lang component seeing as it broke the code

--- client/components/FavLang.tsx
+++ client/components/FavLang.tsx
@@ -1,63 +1,80 @@
-import { useState } from 'react'
-import {
-  useGetFavLanguages,
-  useGetFavLangVotes,
-  useAddFavVote,
-} from '../hooks/useFavVotes.ts'
-import LoadingState from './LoadingState.tsx'
+// import { useState } from 'react'
+// import {
+//   useGetFavLanguages,
+//   useGetFavLangVotes,
+//   useAddFavVote,
+// } from '../hooks/useFavVotes.ts'
+// import LoadingState from './LoadingState.tsx'
 
-function FavLang() {
-  const [checked, setChecked] = useState<string | null>(null)
-  const [hasVoted, setHasVoted] = useState(false)
+// function FavLang() {
+//   const [checked, setChecked] = useState<string | null>(null)
+//   const [hasVoted, setHasVoted] = useState(false)
 
-  const addVotePromise = useAddFavVote()
+//   const addVote = useAddFavVote()
 
-  const { data, isError, isPending } = useGetFavLanguages()
+//   const { data, isError, isPending } = useGetFavLanguages()
 
-  const {
-    data: votes,
-    isError: isVoteError,
-    isPending: isVotePending,
-  } = useGetFavLangVotes()
+//   const {
+//     data: votes,
+//     isError: isVoteError,
+//     isPending: isVotePending,
+//   } = useGetFavLangVotes()
 
-  if (isError || isVoteError) {
-    ;<div>Error Loading Votes</div>
-  }
+//   if (isError || isVoteError) {
+//     return <div>Error Loading Votes</div>
+//   }
 
-  if (isPending || isVotePending) {
-    return <LoadingState />
-  }
+//   if (isPending || isVotePending) {
+//     return <LoadingState />
+//   }
 
-  const handleChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    const selectedId = event.target.value
-    setChecked(selectedId)
-    setHasVoted(true)
-    if (!hasVoted) {
-      try {
-        const addVote = await addVotePromise
-        await addVote.mutateAsync(selectedId)
-      } catch (err) {
-        console.log('Voting Error', err)
-      }
-    }
-  }
+//   const handleChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+//     const selectedId = event.target.value
+//     setChecked(selectedId)
 
-  return (
-    <div>
-      {data?.map((language: { id: string; language: string }) => (
-        <label key={language.id}>
-          <input
-            type="radio"
-            name="language"
-            value="language.id"
-            checked={checked === language.id}
-            onChange={handleChange}
-          />
-          {language.language}
-        </label>
-      ))}
-    </div>
-  )
-}
+//     if (!hasVoted) {
+//       await addVote.mutateAsync(selectedId)
+//       setHasVoted(true)
+//     }
+//   }
 
-export default FavLang
+//   return (
+//     <div>
+//       {data?.map((language: { id: string; language: string }) => (
+//         <label key={language.id}>
+//           <input
+//             type="radio"
+//             name="language"
+//             value={language.id}
+//             checked={checked === language.id}
+//             onChange={handleChange}
+//           />
+//           {language.language}
+//         </label>
+//       ))}
+//       {hasVoted && (
+//         <div>
+//           <h2>Current Vote Count</h2>
+//           <table border={1} cellPadding={10}>
+//             <thead>
+//               <tr>
+//                 <th>Flavour</th>
+//                 <th>Votes</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {votes?.map((vote: { language: string; votes: number }) => (
+//                 <tr key={vote.language}>
+//                   <td>{vote.language}</td>
+//                   <td>{vote.votes}</td>
+//                 </tr>
+//               ))}
+//             </tbody>
+//           </table>
+//         </div>
+//       )}
+//     </div>
+//   )
+// }
+
+// export default FavLang
