eb992bd6bbeda37797991f0a2408d476d09b3596
Matthew Foley <matthewfoley333@gmail.com>
2025-09-25 12:29:22 +1200

    debugginh

--- server/db/seeds/0-clean.js
+++ server/db/seeds/0-clean.js
@@ -3,9 +3,9 @@
  * @returns { Promise<void> }
  */
 export async function seed(knex) {
-  await knex('favourite_language_votes').truncate()
+  await knex('favourite_language_votes').del()
   await knex('favourite_language').truncate()
-  await knex('least_favourite_language_votes').truncate()
+  await knex('least_favourite_language_votes').del()
   await knex('least_favourite_language').truncate()
   await knex('posts').truncate()
   await knex('skills').truncate()
