02012744f5a1f9b3033bd5a458ef2ed67618db11
Matthew Foley <matthewfoley333@gmail.com>
2025-09-26 13:56:23 +1200

    remove the public folder from the multer disk storage

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -7,7 +7,7 @@ const router = Router()
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.resolve('public/images'))
+    cb(null, path.resolve('/images'))
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
