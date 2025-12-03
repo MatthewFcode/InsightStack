cd6e2f33863ddcf14d83a136dc08a91747a962a3
Matthew Foley <matthewfoley333@gmail.com>
2025-12-03 23:53:11 +1300

    add the testing migration

---
+++ server/db/migrations/20251203105035_testing.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.schema.createTable('testing', (table) => {
+    table.increments('id').primary()
+    table.string('message')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  await knex.dropTable('testing')
+}
